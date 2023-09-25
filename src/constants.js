32175194e56ae5f600dba79a7ee0a075b2a6ef07
unknown <jdoujet@gmail.com>
2023-09-24 22:23:09 -0400

    sdk import

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-import { KameleoonUtils } from '../node_modules/@kameleoon/javascript-sdk/dist/index.js';
-
+//import { KameleoonUtils } from '../node_modules/@kameleoon/javascript-sdk/dist/index.js';
+const { KameleoonUtils } = KameleoonSDK;
 // -- Constants values
 export const siteCode = "44r9jgmatl";
 export const featureKey = "demo_web_feature_flag";
