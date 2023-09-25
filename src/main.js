ceaae7192b1b8a66bdda0bda153dc4d0e3bf629e
unknown <jdoujet@gmail.com>
2023-09-24 22:39:30 -0400

    test title FF variable

--- src/main.js
+++ src/main.js
@@ -1,7 +1,32 @@
 //import { KameleoonClient } from '../node_modules/@kameleoon/javascript-sdk/dist/index.js';
-const { KameleoonClient, CustomData, KameleoonUtils } = KameleoonSDK;
+const { KameleoonClient, KameleoonException , CustomData, KameleoonUtils } = KameleoonSDK;
 import { featureKey, siteCode, visitorCode } from "./constants.js";
 
 console.log('test')
 
 const client = new KameleoonClient(siteCode);
+
+
+async function init() {
+    try {
+      await client.initialize();
+      const title = client.getFeatureFlagVariable({
+        visitorCode,
+        featureKey,
+        variableKey: "title_page",
+      }).value
+      //const titleDiv = document.createElement("div");
+      document.querySelector('#app').insertAdjacentHTML("beforeend","<div>"+title+"</div>")
+    } catch (err) {
+      switch (err.type) {
+        case KameleoonException.StorageWrite:
+        // -- Handle error case
+        case KameleoonException.ClientConfiguration:
+        // -- Handle error case
+        default:
+          break;
+      }
+    }
+  }
+  
+  init();
